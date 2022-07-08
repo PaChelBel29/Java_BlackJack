bc46bcd38a4d44df3d5a56077595766a108b560f
PaChelBel29 <51313633+PaChelBel29@users.noreply.github.com>
2022-07-09 07:22:49 +0900

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,16 +1,20 @@
-package 프로젝트;
 import java.util.*;
 
 public class Player{
  private Vector<Card> hand = new Vector<Card>();
 
  public void addCard(Card card) {
- //(A)
- // 카드를 9 public Card get(int item) {
+//(A)
+hand.add(card);
+}
+ 
+public Card get(int item) {
+	return hand.elementAt(item);
  // (B)
  // item 번째 카드를 리턴한다.
  }
  public Vector<Card> getHand() {
+	return hand;
  // (C)
  // 가지고 있는 패를 모두 리턴한다.
  }
